99339a8167a78e5ab3cb13e70bdb3e8804fbf98a
thanhhungtk.th911@gmail.com <thanhhungtk.th911@gmail.com@10b965ac-7a7d-5d27-c79b-78f1308a826a>
2014-04-01 18:46:10 +0000

    Update Ninject cho 3 bộ giao diện

---
+++ trunk/KS.Data/QuanliRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using KS.Core.Interface.Data;
+using KS.Core.Model;
+using KS.Common.Paging;
+using KS.Data.Interface;
+
+namespace KS.Data
+{
+    public class QuanliRepository<T> : Repository<T>, IQuanliRepository<T> where T:PersistentEntity
+    {
+        public override bool AllowDeletedItem
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public QuanliRepository(IDatabaseks databaseks)
+            : base(databaseks)
+        { }
+    }
+}
